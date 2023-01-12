fa222a6319d2c0dd681ea9c71b5d4d29eda7968c
lukinhapdm <lucaspintodemagalhaes@hotmail.com>
2023-01-11 22:48:06 -0300

    Aula: Modulos

---
+++ Test/calc.js
@@ -0,0 +1,9 @@
+var somaFunc = require("./soma.js");
+var subFunc = require("./sub.js");
+var multFunc = require("./mult.js");
+var divFunc = require("./div.js");
+
+console.log(somaFunc(1,2));
+console.log(subFunc(10,5));
+console.log(multFunc(100,10));
+console.log(divFunc(100,10));
