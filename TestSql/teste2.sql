a882e9bd8aa4a17138eb044b809396b0d8f862ce
lukinhapdm <lucaspintodemagalhaes@hotmail.com>
2023-01-17 23:08:36 -0300

    Aula: Manipulando o MySQL

---
+++ TestSql/teste2.sql
@@ -0,0 +1,17 @@
+INSERT INTO usuarios(nome, email, idade) VALUES(
+	"Lucas Magalhaes",
+	"lucaspintodemagalhaes@hotmail.com",
+	26
+);
+
+INSERT INTO usuarios(nome, email, idade) VALUES(
+	"Victor Lima",
+	"victorlima@hotmail.com",
+	30
+);
+
+INSERT INTO usuarios(idade, email, nome) VALUES(
+	1,
+	"teste@hotmail.com",
+	"Teste"
+);
