5cc20435099206e79b1ad854011216524f6c0269
lukinhapdm <lucaspintodemagalhaes@hotmail.com>
2023-01-20 16:13:22 -0300

    Aula: Body Parser

--- Projeto1/index.js
+++ Projeto1/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser')
 const Sequelize = require('sequelize');
 
 
@@ -8,18 +9,21 @@ const Sequelize = require('sequelize');
 	// Template Engine
 	app.engine('handlebars', handlebars.engine({defaultLayout: 'main'})) // main é o tamplate padrão do projeto
 	app.set('view engine', 'handlebars')
-// Conexão com o Banco de Dados MySQL
-const sequelize = new Sequelize('teste', 'root', 'node.js', {
-    host: "localhost",
-    dialect: "mysql"
-})
+	// Body Parser
+	app.use(bodyParser.urlencoded({extended: false}))
+	app.use(bodyParser.json())
+	// Conexão com o Banco de Dados MySQL
+	const sequelize = new Sequelize('teste', 'root', 'node.js', {
+		host: "localhost",
+		dialect: "mysql"
+	})
 
 //Rotas
 app.get("/cadastro", function(req, res){
     res.render("formulario");
 });
 app.post("/adicionar", function(req, res){
-    res.send("Formulario Recebido!");
+    res.send("Titulo: " + req.body.titulo + " Conteudo: " + req.body.conteudo);
 });
 
 app.listen(8081, function(){ //precisar ser a ultima linha do codigo
