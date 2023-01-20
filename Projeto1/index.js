37bfc2c6586cd8af0e04e6823c4c3ce62412aaac
lukinhapdm <lucaspintodemagalhaes@hotmail.com>
2023-01-19 22:56:06 -0300

    Aula: Handlebars

---
+++ Projeto1/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const handlebars = require('express-handlebars')
+const Sequelize = require('sequelize')
+
+
+
+// Configuração
+	// Template Engine
+	app.engine('handlebars', handlebars({defaultLayout: 'main'})) // main é o tamplate padrão do projeto
+	app.set('view engine', 'handlebars')
+// Conexão com o Banco de Dados MySQL
+const sequelize = new Sequelize('teste', 'root', 'node.js', {
+    host: "localhost",
+    dialect: "mysql"
+})
+
+app.listen(8081, function(){ //precisar ser a ultima linha do codigo
+	console.log("The Server is runnig on the url http://localhost:8081");
+});
