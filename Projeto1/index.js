507de804f3b979bd5661dda42e8fc2412161f3d2
lukinhapdm <lucaspintodemagalhaes@hotmail.com>
2023-01-19 23:22:21 -0300

    Aula: Como criar um formulario

--- Projeto1/index.js
+++ Projeto1/index.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const app = express();
-const handlebars = require('express-handlebars')
-const Sequelize = require('sequelize')
-
+const handlebars = require('express-handlebars');
+const Sequelize = require('sequelize');
 
 
 // Configuração
 	// Template Engine
-	app.engine('handlebars', handlebars({defaultLayout: 'main'})) // main é o tamplate padrão do projeto
+	app.engine('handlebars', handlebars.engine({defaultLayout: 'main'})) // main é o tamplate padrão do projeto
 	app.set('view engine', 'handlebars')
 // Conexão com o Banco de Dados MySQL
 const sequelize = new Sequelize('teste', 'root', 'node.js', {
     host: "localhost",
     dialect: "mysql"
 })
+
+//Rotas
+app.get("/cadastro", function(req, res){
+    res.render("formulario");
+});
 
 app.listen(8081, function(){ //precisar ser a ultima linha do codigo
 	console.log("The Server is runnig on the url http://localhost:8081");
