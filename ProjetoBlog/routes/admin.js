fa9462f69856d82a1a1614bf4360d0fe38f353c8
lukinhapdm <lucaspintodemagalhaes@hotmail.com>
2023-01-29 23:12:17 -0300

    Aula: Deletando categorias

--- ProjetoBlog/routes/admin.js
+++ ProjetoBlog/routes/admin.js
@@ -90,4 +90,14 @@ router.post("/categorias/edit", (req, res) => {
 	})
 })
 
+router.post("/categorias/delete", (req, res) => {
+	Categoria.deleteOne({_id: req.body.id}).then(() => {
+		req.flash("success_msg", "Categoria deletada com sucesso!")
+		res.redirect("/admin/categorias")
+	}).catch((erro) => {
+		req.flash("error_msg", "Houve um erro ao deletar a categoria!")
+		res.redirect("/admin/categorias")
+	})
+})
+
 module.exports = router
