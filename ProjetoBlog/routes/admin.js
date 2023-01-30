c7c853d3cfbd29a7066a7f119b6f14a52e99de65
lukinhapdm <lucaspintodemagalhaes@hotmail.com>
2023-01-29 21:56:45 -0300

    Aula: Editando categoria

--- ProjetoBlog/routes/admin.js
+++ ProjetoBlog/routes/admin.js
@@ -15,9 +15,8 @@ router.get('/posts', (req, res) => {
 })
 
 router.get('/categorias', (req, res) => {
-	Categoria.find().sort({date: 'desc'}).then((categorias) => {
-		res.render("admin/categorias", {categorias: categorias.map(categorias => categorias.toJSON()) //A função dentro do objeto é para converter o arquivo para json
-})
+	Categoria.find().sort({date: 'desc'}).lean().then((categorias) => { //O método .lean() faz que as informações retornem como objetos simples (texto)
+		res.render("admin/categorias", {categorias: categorias})
 	}).catch((erro) => {
 		req.flash("error_msg", "Houve um erro ao listar as categorias!")
 		res.redirect("/admin")
@@ -63,6 +62,32 @@ router.post('/categorias/nova', (req, res) => {
 		})
 	}
 	
+})
+
+router.get("/categorias/edit/:id", (req, res) => {
+	Categoria.findOne({_id: req.params.id}).lean().then((categoria) => {
+		res.render("admin/editcategorias", {categoria: categoria})
+	}).catch((erro) => {
+		req.flash("error_msg", "Está categoria não existe!")
+		res.redirect("/admin/categorias")
+	})
+})
+
+router.post("/categorias/edit", (req, res) => {
+	Categoria.findOne({_id: req.body.id}).then((categoria) => {
+		categoria.nome = req.body.nome
+		categoria.slug = req.body.slug
+		categoria.save().then(() => {
+			req.flash("success_msg", "Categoria editada com sucesso!")
+			res.redirect("/admin/categorias")
+		}).catch((erro) => {
+			req.flash("error_msg", "Houve um erro interno ao salvar a edição da categoria!")
+			res.redirect("/admin/categorias")
+		})
+	}).catch((erro) => {
+		req.flash("error_msg", "Houve um erro ao editar a categoria!")
+		res.redirect("/admin/categorias")
+	})
 })
 
 module.exports = router
