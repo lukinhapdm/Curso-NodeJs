0eb25158e7502d334e40ef241a24f43c3b17a8b3
lukinhapdm <lucaspintodemagalhaes@hotmail.com>
2023-01-30 10:32:16 -0300

    Aula: Formulário de postagens

--- ProjetoBlog/routes/admin.js
+++ ProjetoBlog/routes/admin.js
@@ -10,10 +10,6 @@ router.get('/', function(req, res){
 	res.render("admin/index")
 })
 
-router.get('/posts', (req, res) => {
-	res.send("Página de posts")
-})
-
 router.get('/categorias', (req, res) => {
 	Categoria.find().sort({date: 'desc'}).lean().then((categorias) => { //O método .lean() faz que as informações retornem como objetos simples (texto)
 		res.render("admin/categorias", {categorias: categorias})
@@ -99,5 +95,19 @@ router.post("/categorias/delete", (req, res) => {
 		res.redirect("/admin/categorias")
 	})
 })
+
+router.get('/postagens', (req, res) => {
+	res.render("admin/postagens")
+})
+
+router.get('/postagens/add', (req, res) => {
+	Categoria.find().lean().then((categorias) => {
+		res.render("admin/addpostagens", {categorias: categorias})
+	}).catch((erro) => {
+		req.flash("error_msg", "Houve um erro ao carregar o formulário!")
+		res.redirect("/admin")
+	})
+})
+
 
 module.exports = router
